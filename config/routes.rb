a9b64e9f34719d7404497467c9f6aebae2eb503f
maximtimofeev <makscarlos@hotmail.com>
2018-07-25 19:09:33 +0300

    base template and scaffolding users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
+  root 'app#index'
+
+  get 'app/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
