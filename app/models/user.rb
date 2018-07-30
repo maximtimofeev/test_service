605340a2ac3f6ef806e50ab12ee5ff387e245c34
maximtimofeev <makscarlos@hotmail.com>
2018-07-31 01:49:17 +0300

    remove useless mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  after_create :send_admin_mail
-
   has_one :filter
   has_many :comments
   has_many :commented_events, through: :comments, source: :event
@@ -14,10 +12,4 @@ class User < ApplicationRecord
   validates :name, :password, :email, presence: true
   validates :name, length: { minimum: 3, maximum: 16 }
   validates :name, uniqueness: true
-
-  private
-
-  def send_admin_mail
-    UserMailer.send_new_user_message(self).deliver
-  end
 end
